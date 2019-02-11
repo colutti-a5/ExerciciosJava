ca82d3eb0e0837deacbd5852f5fc1c8d5c98e748
Vinicius Colutti <vcolutti@a5-solutions.local>
2019-02-11 16:05:40 -0200

    valida cpf e cnpj

---
+++ ValidaCelular/src/ValidaCelular.java
@@ -0,0 +1,14 @@
+
+public class ValidaCelular {
+	
+	public static boolean validar(String telefoneCelular) {
+		
+	}
+	
+	private static boolean validarTelefone(String telefone) {
+		
+	}
+	
+	
+
+}
